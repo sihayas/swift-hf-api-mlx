2471d57659a9572f8cdba628d39e3b494c3f80d3
sihayas <sihayas@icloud.com>
2026-04-13 12:05:30 -0400

    Change mlx-swift-lm dependency revision
    Updated the revision of mlx-swift-lm dependency.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // TODO: Switch from this pinned revision to a major-version dependency once mlx-swift-lm publishes a release that includes PR #118.
         .package(
             url: "https://github.com/ml-explore/mlx-swift-lm.git",
-            revision: "8c9dd6391139242261bcf27d253c326f9cf2d567"
+            revision: "67b146eba8cad6880d30dc3a883eb80cee29adca"
         ),
         .package(url: "https://github.com/DePasqualeOrg/swift-hf-api", from: "0.2.2"),
     ],
